d26a0d911f50175a0a55d948cc7b303a226bdb51
Mari Medici <marilau.medici@gmail.com>
2021-09-21 20:29:05 -0300

    front upload

--- src/main/java/arq/soft/front/controllers/AbstractController.java
+++ src/main/java/arq/soft/front/controllers/AbstractController.java
@@ -4,8 +4,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 public abstract class AbstractController {
 	
-	private WebClient webClient = WebClient.create("http://localhost:8081");
-	//private WebClient webClient = WebClient.create("https://arqsoftlibreback.herokuapp.com");
+	//private WebClient webClient = WebClient.create("http://localhost:8081");
+	private WebClient webClient = WebClient.create("https://arqsoftlibreback.herokuapp.com");
 
 	public WebClient getWebClient() {
 		return webClient;
